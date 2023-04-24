1db54104dc4036591c1fe8c09e4a2659d057563f
Fagner Avila <fagner.avila@gmail.com>
2023-04-24 01:42:13 -0300

    initial commit

---
+++ src/main/java/com/avila/pgto/pgtoNA/PgtoNaApplication.java
@@ -0,0 +1,13 @@
+package com.avila.pgto.pgtoNA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgtoNaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgtoNaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avila/pgto/pgtoNA/PgtoNaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avila.pgto.pgtoNA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgtoNaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
