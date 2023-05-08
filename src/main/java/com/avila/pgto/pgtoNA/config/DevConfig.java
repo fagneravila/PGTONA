399cff04d15aa0e2604e3ac450c22b9dcf8f6907
Fagner Avila <fagner.avila@gmail.com>
2023-05-07 23:17:24 -0300

    pegando variavel do property

--- src/main/java/com/avila/pgto/pgtoNA/config/DevConfig.java
+++ src/main/java/com/avila/pgto/pgtoNA/config/DevConfig.java
@@ -2,7 +2,6 @@ package com.avila.pgto.pgtoNA.config;
 
 import com.avila.pgto.pgtoNA.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,22 +9,14 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
-
-    @Value("spring.jpa.hibernate.ddl-auto=create")
-    private String strategy;
-
-
     @Bean
     public boolean instantiateDataBase() throws ParseException {
-     if(!"create".equals(strategy)) {
-         return false;
-     }
-        dbService.InstateateTestDataBase();
+      dbService.InstateateTestDataBase();
         return true;
     }
 
